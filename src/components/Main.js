3ca4f850dbc251086f1b3cc8f243e7b6d1a54a06
shoxruzbek_444 <sh.axmadov@fincube.uz>
2024-11-18 18:10:49 +0500

    fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ function Main() {
         <div>
             <CCard className="text-center" style= {{borderBottom:"none"}}>
                 <CCardHeader>
-                    <CNav variant="pills" className="card-header-pills">
+                    <CNav  variant="pills" className="card-header-pills flex-nav" >
                         <CNavItem>
                             <CNavLink href="#" active>Главная страница</CNavLink>
                         </CNavItem>
@@ -26,37 +26,37 @@ function Main() {
                     <Image src={logo} style={{ width: "100px", marginTop: "30px", marginBottom: "50px", borderRadius: "50%" }} />
                     <CCardTitle>Иброхим Садиев Ахмадович</CCardTitle>
                     <CCardText style={{ maxWidth: "400px", margin: "20px auto", textAlign: "left" }}>
-                        <span style={{ display: "flex", fontSize: "1em", justifyContent: "space-between" }}>
+                        <span className='responsive-text' style={{ display: "flex", justifyContent: "space-between" }}>
                             <span>Дата рождения:</span>
                             <span>17 Февраль 1974</span>
                         </span>
-                        <span style={{ display: "flex", fontSize: "1em", justifyContent: "space-between" }}>
+                        <span className='responsive-text' style={{ display: "flex", justifyContent: "space-between" }}>
                             <span>Почта:</span>
                             <span>Ibrokhim.sadiev@mail.ru</span>
                         </span>
-                        <span style={{ display: "flex", fontSize: "1em", justifyContent: "space-between" }}>
+                        <span className='responsive-text' style={{ display: "flex", justifyContent: "space-between" }}>
                             <span>Телефон номер:</span>
-                            <span style={{ fontSize: "16px", display: "flex", flexDirection: "column" }}>
+                            <span style={{ display: "flex", flexDirection: "column" }}>
                                 <span>+998 (94) 540-77-77</span>
                                 <span>+998 (93) 656-88-88</span>
                             </span>
                         </span>
                     </CCardText>
-                    <div style={{ display: "flex", flexDirection: "column", width: "100%", maxWidth: "300px", margin: "auto", gap: "10px" }}>
-                        <CButton style={{ fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="https://t.me/+998945407777">
+                    <div style={{ display: "flex", flexDirection: "column", maxWidth: "500px", margin: "auto", gap: "10px" }}>
+                        <CButton className='responsive-button' style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="https://t.me/+998945407777">
                             Telegram
                             <CIcon icon={cibTelegramPlane} size='xl' />
                         </CButton>
-                        <CButton style={{ fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="#">
+                        <CButton className='responsive-button' style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="#">
                             Facebook
                             <CIcon icon={cibFacebookF} size='xl' />
                         </CButton>
 
-                        <CButton style={{ fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="#">
+                        <CButton className='responsive-button' style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="#">
                             Instagram
                             <CIcon icon={cibInstagram} size='xl' />
                         </CButton>
-                        <CButton style={{ fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="#">
+                        <CButton className='responsive-button' style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "5%" }} color="primary" href="#">
                             Linkedin
                             <CIcon icon={cibLinkedinIn} size='xl' />
                         </CButton>
